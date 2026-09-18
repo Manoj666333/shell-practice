7bea0be46a57f670f4ffb85915b2335e47af40bc
Manoj666333 <siraparapumanoj@gmail.com>
2026-09-18 21:03:11 +0530

    first

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,4 +1,6 @@
 !#/bin/bash
-
-echo "Hi , i want to learn devops "
+person1=$1
+person2=$2
+echo "$person1 , i want to learn devops "
 echo "okay,then learn but i don't know where to start"
+echo "$person2"
